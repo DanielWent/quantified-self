6d46bdb336ff9d59185e074901d6acc9f9538b6e
DanielWent <109043916+DanielWent@users.noreply.github.com>
2026-08-26 07:27:25 +0100

    Update config.py

--- garmin/config.py
+++ garmin/config.py
@@ -9,5 +9,5 @@ GARMIN_TOKENS_PATH = os.getenv("GARMIN_TOKENS_PATH", ".garmin_tokens")
 GOOGLE_SERVICE_ACCOUNT_JSON = os.getenv("GOOGLE_SERVICE_ACCOUNT_JSON")
 GOOGLE_DRIVE_FOLDER_ID = os.getenv("GOOGLE_DRIVE_FOLDER_ID")
 
-GARMIN_DATA_FILENAME = "garmin_data.csv"
+GARMIN_DAILY_FILENAME = "garmin_daily_summary.csv"
 GARMIN_ACTIVITIES_FILENAME = "garmin_activities.csv"
