1beacc5fd36dd53183a28409ac5b7840fd1d20de
DanielWent <109043916+DanielWent@users.noreply.github.com>
2026-08-26 18:42:43 +0100

    Update config.py

--- garmin/config.py
+++ garmin/config.py
@@ -3,10 +3,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+GARMIN_TOKENS = os.getenv("GARMIN_TOKENS")
 GARMIN_EMAIL = os.getenv("GARMIN_EMAIL")
 GARMIN_PASSWORD = os.getenv("GARMIN_PASSWORD")
-GARMIN_TOKEN = os.getenv("GARMIN_TOKEN")
-GOOGLE_SHEETS_CREDENTIALS = os.getenv("GOOGLE_SHEETS_CREDENTIALS")
-GOOGLE_SPREADSHEET_KEY = os.getenv("GOOGLE_SPREADSHEET_KEY")
+GOOGLE_DRIVE_CREDENTIALS = os.getenv("GOOGLE_DRIVE_CREDENTIALS")
+GOOGLE_DRIVE_FOLDER_ID = os.getenv("GOOGLE_DRIVE_FOLDER_ID")
 
-DAYS_TO_SYNC = int(os.getenv("DAYS_TO_SYNC", os.getenv("DAYS", "7")))
+try:
+    DAYS_TO_SYNC = int(os.getenv("DAYS_TO_SYNC", 7))
+except (ValueError, TypeError):
+    DAYS_TO_SYNC = 7
