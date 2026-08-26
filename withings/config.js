44b509bb3e87bcbe25c6d4d59f9b790a51296b7c
DanielWent <109043916+DanielWent@users.noreply.github.com>
2026-08-26 20:13:49 +0100

    Update config.js

--- withings/config.js
+++ withings/config.js
@@ -4,7 +4,7 @@ const config = {
   clientId: process.env.WITHINGS_CLIENT_ID,
   clientSecret: process.env.WITHINGS_CLIENT_SECRET,
   refreshToken: process.env.WITHINGS_REFRESH_TOKEN,
-  googleDriveCredentials: process.env.GOOGLE_DRIVE_CREDENTIALS,
+  googleDriveCredentials: process.env.GOOGLE_SERVICE_ACCOUNT_JSON || process.env.GOOGLE_DRIVE_CREDENTIALS,
   googleDriveFolderId: process.env.GOOGLE_DRIVE_FOLDER_ID,
   daysToSync: parseInt(process.env.DAYS_TO_SYNC, 10) || 7
 };
