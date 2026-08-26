579f7daacd1f8688beab61fd74fca18b132fc3db
DanielWent <109043916+DanielWent@users.noreply.github.com>
2026-08-26 18:43:51 +0100

    Update config.js

--- withings/config.js
+++ withings/config.js
@@ -1,12 +1,10 @@
 require('dotenv').config();
 
-const parsedDays = parseInt(process.argv[2] || process.env.DAYS_TO_SYNC || process.env.DAYS || '7', 10);
-
 module.exports = {
   clientId: process.env.WITHINGS_CLIENT_ID,
   clientSecret: process.env.WITHINGS_CLIENT_SECRET,
   refreshToken: process.env.WITHINGS_REFRESH_TOKEN,
-  spreadsheetKey: process.env.GOOGLE_SPREADSHEET_KEY,
-  googleCredentials: process.env.GOOGLE_SHEETS_CREDENTIALS,
-  days: isNaN(parsedDays) ? 7 : parsedDays,
+  googleDriveCredentials: process.env.GOOGLE_DRIVE_CREDENTIALS,
+  googleDriveFolderId: process.env.GOOGLE_DRIVE_FOLDER_ID,
+  daysToSync: parseInt(process.env.DAYS_TO_SYNC, 10) || 7
 };
