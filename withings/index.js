7ee0f9a8748ecef833f5e6ab2d4242f292fa851c
DanielWent <109043916+DanielWent@users.noreply.github.com>
2026-08-26 07:54:47 +0100

    Update index.js

--- withings/index.js
+++ withings/index.js
@@ -2,12 +2,13 @@ const config = require('./config');
 const { getAccessToken, getMeasurements, getSleepSummary, updateWithingsSheet } = require('./utils');
 
 async function run() {
-  console.log(`Starting Withings sync for the past ${config.days} days...`);
+  const daysToFetch = config.days;
+  console.log(`Starting Withings sync for the past ${daysToFetch} days...`);
   
   const accessToken = await getAccessToken(config.clientId, config.clientSecret, config.refreshToken);
   
   const endTimestamp = Math.floor(Date.now() / 1000);
-  const startTimestamp = endTimestamp - (config.days * 24 * 60 * 60);
+  const startTimestamp = endTimestamp - (daysToFetch * 24 * 60 * 60);
 
   const [measurements, sleepData] = await Promise.all([
     getMeasurements(accessToken, startTimestamp, endTimestamp),
@@ -15,7 +16,7 @@ async function run() {
   ]);
 
   await updateWithingsSheet(config.spreadsheetKey, config.googleCredentials, measurements, sleepData);
-  console.log(`Successfully synced Withings data for ${config.days} days.`);
+  console.log(`Successfully synced Withings data for ${daysToFetch} days.`);
 }
 
 run().catch((err) => {
